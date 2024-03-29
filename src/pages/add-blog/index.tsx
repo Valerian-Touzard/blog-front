6bd1c203431e6fdff1feca9d0d8c9ae03b7b5549
vtouzard <vtouzard244@gmail.com>
2024-03-29 11:13:40 +0100

    feat: Mise en place du formulaire pour ajouter un blog

--- src/pages/add-blog/index.tsx
+++ src/pages/add-blog/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import classes from "./styles.module.css";
 
 const AddNewBlog = () => {
   return (
-    <div>
+    <div className={classes.wrapper}>
       <h1>Add a blog</h1>
+      <div className={classes.formWrapper}>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          placeholder="Enter Blog Title"
+        />
+        <textarea
+          id="description"
+          name="description"
+          placeholder="Enter Blog Description"
+        />
+        <button>Add new Blog</button>
+      </div>
     </div>
   );
 };
