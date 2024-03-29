e38247531d18bbc1738bacf03ff963253a86feea
vtouzard <vtouzard244@gmail.com>
2024-03-29 11:21:25 +0100

    feat: Récupération des changements dans le formulaires par l'utilisateur

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,15 +1,28 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 // Le type de retour pour le context
-type GlobalContextType = {};
+type GlobalContextType = {
+  formData: FormDataType
+  setFormData: React.Dispatch<React.SetStateAction<FormDataType>>;
+};
+
+type FormDataType = {
+  title: string;
+  descrition: string;
+};
 
 export const GlobalContext = createContext<GlobalContextType | undefined>(
   undefined
 );
 
 export const GlobalState = ({ children }: React.PropsWithChildren) => {
+  const [formData, setFormData] = useState<FormDataType>({
+    title: "",
+    descrition: "",
+  });
+
   return (
-    <GlobalContext.Provider value={undefined}>
+    <GlobalContext.Provider value={{formData, setFormData}}>
       {children}
     </GlobalContext.Provider>
   );
--- src/pages/add-blog/index.tsx
+++ src/pages/add-blog/index.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import classes from "./styles.module.css";
+import { useGlobalState } from "../../context";
 
 const AddNewBlog = () => {
+  const { formData, setFormData } = useGlobalState();
+
   return (
     <div className={classes.wrapper}>
       <h1>Add a blog</h1>
       <div className={classes.formWrapper}>
         <input
+          onChange={(e) => setFormData({ ...formData, title: e.target.value })}
           id="title"
           name="title"
           type="text"
+          value={formData.title}
           placeholder="Enter Blog Title"
         />
         <textarea
+          onChange={(e) =>
+            setFormData({ ...formData, descrition: e.target.value })
+          }
           id="description"
           name="description"
+          value={formData.descrition}
           placeholder="Enter Blog Description"
         />
         <button>Add new Blog</button>
